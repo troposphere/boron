393bf38b2d38898c7928c0881960cdc4a69ec2f3
Sankha Narayan Guria <sankha93@gmail.com>
2016-07-28 22:39:50 +0530

    top level module docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,18 @@ extern crate hyper;
 extern crate url;
 extern crate regex;
 
+/// Boron app server methods and utilities
 pub mod server;
+
+/// HTTP response representation in Boron
 pub mod response;
+
+/// HTTP request representation in Boron
 pub mod request;
+
+/// Traits to implement middleware types
 pub mod middleware;
+
+/// Router logic to dispatch requests
 pub mod router;
-pub mod matcher;
+mod matcher;
